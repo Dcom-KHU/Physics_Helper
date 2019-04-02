bb6a7ab90d55746825e4d849edd582a455d71533
서주원 <jwseo001@khu.ac.kr>
2019-04-02 20:55:54 +0900

    Fix session related errors

--- views/helper/render.js
+++ views/helper/render.js
@@ -1,15 +1,20 @@
 const fs=require('fs')
 
 exports.Render=(req,res)=>{
-    let expNo=Number(req.params.expNo)
-    if(expNo/10<1){
-        expNo='0'+expNo
-    }
-    fs.readFile('./static/html/exp'+expNo+'.html',(err,data)=>{
-        if(err){
-            console.log(err)
+    if(req.session.verified){
+        let expNo=Number(req.params.expNo)
+        if(expNo/10<1){
+            expNo='0'+expNo
         }
-        res.writeHead(200,{'Content-Type':'text/html'})
-        res.end(data)
-    })
+        fs.readFile('./static/html/exp'+expNo+'.html',(err,data)=>{
+            if(err){
+                console.log(err)
+            }
+            res.writeHead(200,{'Content-Type':'text/html'})
+            res.end(data)
+        })
+    }
+    else{
+        res.redirect('/')
+    }
 }
--- views/list.js
+++ views/list.js
@@ -1,14 +1,19 @@
 const fs=require('fs')
 
 exports.List=(req,res)=>{
-    const Render=()=>{
-        fs.readFile('./static/html/list.html',(err,data)=>{
-            if(err){
-                console.log(err)
-            }
-            res.writeHead(200,{'Content-Type':'text/html'})
-            res.end(data)
-        })
+    if(req.session.verified){
+        const Render=()=>{
+            fs.readFile('./static/html/list.html',(err,data)=>{
+                if(err){
+                    console.log(err)
+                }
+                res.writeHead(200,{'Content-Type':'text/html'})
+                res.end(data)
+            })
+        }
+        Render()
     }
-    Render()
+    else{
+        res.redirect('/')
+    }
 }
