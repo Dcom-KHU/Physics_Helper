4831a46ac9ee47fc9c084119b43bb3e999265092
서주원 <jwseo001@khu.ac.kr>
2019-04-02 19:19:54 +0900

    Pubilsh exp01.html and Implement render.js

--- views/helper/render.js
+++ views/helper/render.js
@@ -1,3 +1,15 @@
-exports.Render=(req,res)=>{
+const fs=require('fs')
 
+exports.Render=(req,res)=>{
+    let expNo=Number(req.params.expNo)
+    if(expNo/10<1){
+        expNo='0'+expNo
+    }
+    fs.readFile('./static/html/exp'+expNo+'.html',(err,data)=>{
+        if(err){
+            console.log(err)
+        }
+        res.writeHead(200,{'Content-Type':'text/html'})
+        res.end(data)
+    })
 }
